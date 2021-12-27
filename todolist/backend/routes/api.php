70e35f3452c346043f86311d8cdf723c91c592de
unknown <mohamed344>
2021-12-27 16:22:28 +0800

    mohamed

---
+++ todolist/backend/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/items', 'App\Http\Controllers\ItemController@index');
+Route::prefix('/item')->group(function(){
+    Route::post('/store', 'App\Http\Controllers\ItemController@store');
+    Route::put('/{id}', 'App\Http\Controllers\ItemController@update');
+    Route::delete('/{id}', 'App\Http\Controllers\ItemController@destroy');
+});
